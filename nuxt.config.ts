a5c8e1bb7116485783f813ce52852bb3dd0e902c
stephaneBalelos <stephanedondyas2@gmail.com>
2024-10-15 00:03:18 +0200

    Added Nuxt UI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['pruvious'],
+  modules: ['pruvious', '@nuxt/ui'],
 
   pruvious: {
     jwt: {
