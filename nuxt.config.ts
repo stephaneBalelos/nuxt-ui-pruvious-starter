c1f6cea4ea4121dbe6ec5d52270742a2f4809e18
stephaneBalelos <stephanedondyas2@gmail.com>
2024-10-15 00:01:16 +0200

    Initial Commit

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: ['pruvious'],
+
+  pruvious: {
+    jwt: {
+      secretKey: process.env.NUXT_JWT_SECRET_KEY,
+    },
+    database: process.env.NUXT_PRUVIOUS_DATABASE,
+  }
+})
